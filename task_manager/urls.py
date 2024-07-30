f410810da62da7e2d5fda81f3663bdc24abd8751
sergi <qasergievich@gmail.com>
2024-07-31 01:17:13 +0300

    venv, роут "/", django-bootstrap5, i18n step(1, 2)

---
+++ task_manager/urls.py
@@ -0,0 +1,27 @@
+"""
+URL configuration for task_manager project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+from django.views.i18n import set_language
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('set-language/', set_language, name='set_language'),
+    path("i18n/", include("django.conf.urls.i18n")),
+]
